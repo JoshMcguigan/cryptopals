10edb5f9228da52bee459165bae14135a6e7887e
Josh Mcguigan <joshmcg88@gmail.com>
2025-11-24 20:58:02 -0800

    set 1 challenge 6

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::num::NonZero;
+
 pub mod analysis;
 
 /// Performs repeating key XOR.
@@ -20,9 +22,48 @@ pub struct PossibleBreak {
 /// Returns the most likely plaintext.
 pub fn break_xor(
     ciphertext: &[u8],
-    #[expect(unused, reason = "not yet implemented")] possible_key_len: impl std::ops::RangeBounds<
-        usize,
-    >,
+    keysize: usize,
+    plaintext_scorer: fn(&[u8]) -> f64,
+) -> PossibleBreak {
+    let mut ret = (0..keysize)
+        .map(|i| {
+            let chunk = ciphertext
+                .iter()
+                .copied()
+                .skip(i)
+                .step_by(keysize)
+                .collect::<Vec<u8>>();
+            (i, break_single_byte_xor(&chunk, plaintext_scorer))
+        })
+        .fold(
+            PossibleBreak {
+                score: 0.,
+                possible_key: vec![],
+                possible_plaintext: vec![0; ciphertext.len()],
+            },
+            |mut acc, (i, e)| {
+                acc.score += e.score;
+                acc.possible_key.push(e.possible_key[0]);
+
+                e.possible_plaintext
+                    .iter()
+                    .copied()
+                    .enumerate()
+                    .for_each(|(j, b)| {
+                        acc.possible_plaintext[i + j * keysize] = b;
+                    });
+
+                acc
+            },
+        );
+
+    ret.score /= keysize as f64;
+
+    ret
+}
+
+pub fn break_single_byte_xor(
+    ciphertext: &[u8],
     plaintext_scorer: fn(&[u8]) -> f64,
 ) -> PossibleBreak {
     (u8::MIN..=u8::MAX)
@@ -38,4 +79,36 @@ pub fn break_xor(
             possible_plaintext: plaintext,
         })
         .expect("there must be some max because we iterated over all possible keys")
+}
+
+/// Find likely XOR keysize assuming ASCII-ish plaintext.
+pub fn find_xor_keysize(ciphertext: &[u8], max_key_len: NonZero<usize>) -> NonZero<usize> {
+    (1..=max_key_len.into())
+        .map(|keysize| {
+            let first_chunk = &ciphertext[0..keysize];
+
+            let bits_diff_per_byte = ciphertext
+                .chunks_exact(keysize)
+                .skip(1)
+                .map(|n_th_chunk| {
+                    first_chunk
+                        .iter()
+                        .copied()
+                        .zip(n_th_chunk)
+                        .map(|(b1, b2)| (b1 ^ b2).count_ones() as usize)
+                        .sum::<usize>() as f64
+                        / keysize as f64
+                })
+                .sum::<f64>()
+                / (ciphertext.len() / keysize) as f64;
+
+            (keysize, bits_diff_per_byte)
+        })
+        .find(|(_keysize, bits_diff_per_byte)| *bits_diff_per_byte < 2.9)
+        // TODO
+        // This function should return option here.
+        .expect("must find key size")
+        .0
+        .try_into()
+        .expect("key size known to be non-zero")
 }
--- tests/set_01.rs
+++ tests/set_01.rs
@@ -1,3 +1,4 @@
+use cryptopals::find_xor_keysize;
 use data_encoding::{BASE64, HEXLOWER};
 
 /// No code is implemented for this challenge - but it confirms that the `data_encoding`
@@ -46,11 +47,10 @@ fn challenge_03() {
         possible_key: key,
         possible_plaintext: plaintext,
         ..
-    } = cryptopals::break_xor(
+    } = cryptopals::break_single_byte_xor(
         &HEXLOWER
             .decode(ciphertext_hex.as_bytes())
             .expect("input is valid hex"),
-        1..=1,
         cryptopals::analysis::plaintext_scorer_english_prose,
     );
     assert_eq!(expected_key, key);
@@ -69,11 +69,10 @@ fn challenge_04() {
                 score,
                 possible_plaintext,
                 ..
-            } = cryptopals::break_xor(
+            } = cryptopals::break_single_byte_xor(
                 &HEXLOWER
                     .decode(ciphertext_hex_line.as_bytes())
                     .expect("input is valid hex"),
-                1..=1,
                 cryptopals::analysis::plaintext_scorer_english_prose,
             );
 
@@ -97,4 +96,116 @@ fn challenge_05() {
         expected_ciphertext_hex,
         HEXLOWER.encode(&cryptopals::xor(plaintext.as_bytes(), b"ICE"))
     )
+}
+
+#[test]
+fn challenge_06() {
+    let ciphertext_base64 = include_str!("challenge-data/6.txt");
+    let ciphertext = ciphertext_base64
+        .lines()
+        .flat_map(|line| {
+            BASE64
+                .decode(line.as_bytes())
+                .expect("input should be valid base64")
+        })
+        .collect::<Vec<u8>>();
+
+    let expected_key = vec![
+        84, 101, 114, 109, 105, 110, 97, 116, 111, 114, 32, 88, 58, 32, 66, 114, 105, 110, 103, 32,
+        116, 104, 101, 32, 110, 111, 105, 115, 101,
+    ];
+    let expected_plaintext = "I'm back and I'm ringin' the bell \n\
+                              A rockin' on the mike while the fly girls yell \n\
+                              In ecstasy in the back of me \n\
+                              Well that's my DJ Deshay cuttin' all them Z's \n\
+                              Hittin' hard and the girlies goin' crazy \n\
+                              Vanilla's on the mike, man I'm not lazy. \n\
+                              \n\
+                              I'm lettin' my drug kick in \n\
+                              It controls my mouth and I begin \n\
+                              To just let it flow, let my concepts go \n\
+                              My posse's to the side yellin', Go Vanilla Go! \n\
+                              \n\
+                              Smooth 'cause that's the way I will be \n\
+                              And if you don't give a damn, then \n\
+                              Why you starin' at me \n\
+                              So get off 'cause I control the stage \n\
+                              There's no dissin' allowed \n\
+                              I'm in my own phase \n\
+                              The girlies sa y they love me and that is ok \n\
+                              And I can dance better than any kid n' play \n\
+                              \n\
+                              Stage 2 -- Yea the one ya' wanna listen to \n\
+                              It's off my head so let the beat play through \n\
+                              So I can funk it up and make it sound good \n\
+                              1-2-3 Yo -- Knock on some wood \n\
+                              For good luck, I like my rhymes atrocious \n\
+                              Supercalafragilisticexpialidocious \n\
+                              I'm an effect and that you can bet \n\
+                              I can take a fly girl and make her wet. \n\
+                              \n\
+                              I'm like Samson -- Samson to Delilah \n\
+                              There's no denyin', You can try to hang \n\
+                              But you'll keep tryin' to get my style \n\
+                              Over and over, practice makes perfect \n\
+                              But not if you're a loafer. \n\
+                              \n\
+                              You'll get nowhere, no place, no time, no girls \n\
+                              Soon -- Oh my God, homebody, you probably eat \n\
+                              Spaghetti with a spoon! Come on and say it! \n\
+                              \n\
+                              VIP. Vanilla Ice yep, yep, I'm comin' hard like a rhino \n\
+                              Intoxicating so you stagger like a wino \n\
+                              So punks stop trying and girl stop cryin' \n\
+                              Vanilla Ice is sellin' and you people are buyin' \n\
+                              'Cause why the freaks are jockin' like Crazy Glue \n\
+                              Movin' and groovin' trying to sing along \n\
+                              All through the ghetto groovin' this here song \n\
+                              Now you're amazed by the VIP posse. \n\
+                              \n\
+                              Steppin' so hard like a German Nazi \n\
+                              Startled by the bases hittin' ground \n\
+                              There's no trippin' on mine, I'm just gettin' down \n\
+                              Sparkamatic, I'm hangin' tight like a fanatic \n\
+                              You trapped me once and I thought that \n\
+                              You might have it \n\
+                              So step down and lend me your ear \n\
+                              '89 in my time! You, '90 is my year. \n\
+                              \n\
+                              You're weakenin' fast, YO! and I can tell it \n\
+                              Your body's gettin' hot, so, so I can smell it \n\
+                              So don't be mad and don't be sad \n\
+                              'Cause the lyrics belong to ICE, You can call me Dad \n\
+                              You're pitchin' a fit, so step back and endure \n\
+                              Let the witch doctor, Ice, do the dance to cure \n\
+                              So come up close and don't be square \n\
+                              You wanna battle me -- Anytime, anywhere \n\
+                              \n\
+                              You thought that I was weak, Boy, you're dead wrong \n\
+                              So come on, everybody and sing this song \n\
+                              \n\
+                              Say -- Play that funky music Say, go white boy, go white boy go \n\
+                              play that funky music Go white boy, go white boy, go \n\
+                              Lay down and boogie and play that funky music till you die. \n\
+                              \n\
+                              Play that funky music Come on, Come on, let me hear \n\
+                              Play that funky music white boy you say it, say it \n\
+                              Play that funky music A little louder now \n\
+                              Play that funky music, white boy Come on, Come on, Come on \n\
+                              Play that funky music \n";
+
+    let keysize = find_xor_keysize(&ciphertext, 60.try_into().expect("value is non-zero"));
+    assert_eq!(expected_key.len(), keysize.into());
+
+    let cryptopals::PossibleBreak {
+        possible_key: key,
+        possible_plaintext: plaintext,
+        ..
+    } = cryptopals::break_xor(
+        &ciphertext,
+        keysize.into(),
+        cryptopals::analysis::plaintext_scorer_english_prose,
+    );
+    assert_eq!(expected_key, key);
+    assert_eq!(expected_plaintext, String::from_utf8_lossy(&plaintext));
 }
