a0ddf6e1ef9b022809cf16fb1c7cb2502b1b94d9
Josh Mcguigan <joshmcg88@gmail.com>
2025-11-25 19:25:58 -0800

    set 2 challenge 10

--- src/aes.rs
+++ src/aes.rs
@@ -23,3 +23,33 @@ pub fn ecb_decrypt(key: &Key<Aes128>, ciphertext: &[u8]) -> Vec<u8> {
 
     plaintext
 }
+
+/// Decrypts AES CBC. Ciphertext length should be a multiple of the block
+/// size, trailing bytes which don't fill a block are ignored.
+///
+/// `iv` length must equal the block size.
+///
+/// Padding bytes are removed per PKCS#7.
+pub fn cbc_decrypt(key: &Key<Aes128>, ciphertext: &[u8], iv: &[u8]) -> Vec<u8> {
+    let cipher = Aes128::new(key);
+    let mut plaintext = vec![0; ciphertext.len()];
+    for (i, cipher_block) in ciphertext.chunks_exact(16).enumerate() {
+        cipher.decrypt_block_b2b(
+            cipher_block.into(),
+            (&mut plaintext[(16 * i)..(16 * (i + 1))]).into(),
+        );
+
+        let previous_cipher_block = if i == 0 {
+            iv
+        } else {
+            &ciphertext[(16 * (i - 1))..(16 * i)]
+        };
+        for j in 0..16 {
+            plaintext[16 * i + j] ^= previous_cipher_block[j];
+        }
+    }
+
+    pkcs7_remove(&mut plaintext);
+
+    plaintext
+}
--- tests/set_02.rs
+++ tests/set_02.rs
@@ -1,6 +1,7 @@
-use cryptopals::pad::pkcs7;
+use cryptopals::{aes, pad::pkcs7};
 
-use insta::assert_debug_snapshot;
+use data_encoding::BASE64;
+use insta::{assert_debug_snapshot, assert_snapshot};
 
 #[test]
 fn challenge_09() {
@@ -11,4 +12,23 @@ fn challenge_09() {
 
     assert_eq!(20, text.len());
     assert_debug_snapshot!(text);
+}
+
+#[test]
+fn challenge_10() {
+    let ciphertext_base64 = include_str!("challenge-data/10.txt");
+    let ciphertext = ciphertext_base64
+        .lines()
+        .flat_map(|line| {
+            BASE64
+                .decode(line.as_bytes())
+                .expect("input should be valid base64")
+        })
+        .collect::<Vec<u8>>();
+
+    let key = b"YELLOW SUBMARINE";
+
+    let plaintext = aes::cbc_decrypt(key.into(), &ciphertext, &[0; 16]);
+
+    assert_snapshot!(String::from_utf8_lossy(&plaintext));
 }
