651b7c66464fe08a3cda6c068936bc2ba28489c2
Josh Mcguigan <joshmcg88@gmail.com>
2025-11-26 16:35:40 -0800

    cbc encrypt

--- src/aes.rs
+++ src/aes.rs
@@ -43,6 +43,30 @@ pub fn ecb_decrypt(key: &Key<Aes128>, ciphertext: &[u8]) -> Vec<u8> {
     plaintext
 }
 
+/// Encrypts with AES CBC.
+///
+/// `iv` length must equal the block size.
+///
+/// Padding bytes are added per PKCS#7.
+pub fn cbc_encrypt(key: &Key<Aes128>, plaintext: &[u8], iv: &[u8]) -> Vec<u8> {
+    let mut cipher = Aes128::new(key);
+    let mut ciphertext = Vec::from(plaintext);
+    pkcs7(&mut ciphertext, AES_BLOCK_SIZE);
+
+    let mut previous_cipher_block = iv;
+
+    for cipher_block in ciphertext.chunks_exact_mut(AES_BLOCK_SIZE as usize) {
+        for i in 0..(AES_BLOCK_SIZE as usize) {
+            cipher_block[i] ^= previous_cipher_block[i];
+        }
+        cipher.encrypt_block_mut(cipher_block.into());
+
+        previous_cipher_block = cipher_block;
+    }
+
+    ciphertext
+}
+
 /// Decrypts AES CBC. Ciphertext length should be a multiple of the block
 /// size, trailing bytes which don't fill a block are ignored.
 ///
@@ -73,7 +97,7 @@ pub fn cbc_decrypt(key: &Key<Aes128>, ciphertext: &[u8], iv: &[u8]) -> Vec<u8> {
 
 #[cfg(test)]
 mod tests {
-    use super::{ecb_decrypt, ecb_encrypt};
+    use super::{cbc_decrypt, cbc_encrypt, ecb_decrypt, ecb_encrypt};
 
     #[test]
     fn ecb_round_trip() {
@@ -83,6 +107,18 @@ mod tests {
         assert_eq!(
             plaintext[..],
             ecb_decrypt(key, &ecb_encrypt(key, plaintext))[..]
+        );
+    }
+
+    #[test]
+    fn cbc_round_trip() {
+        let key = b"YELLOW SUBMARINE".into();
+        let iv = b"RANDOM IV WOWOWW";
+        let plaintext = b"what time is it game time, what time is it game time";
+
+        assert_eq!(
+            plaintext[..],
+            cbc_decrypt(key, &cbc_encrypt(key, plaintext, iv), iv)[..]
         );
     }
 }
