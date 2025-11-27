f7e3e77be57722bcdf16bb77157a3465c6e6ccb0
Josh Mcguigan <joshmcg88@gmail.com>
2025-11-26 21:11:32 -0800

    set 2 challenge 12

--- tests/set_02.rs
+++ tests/set_02.rs
@@ -1,6 +1,9 @@
 use std::collections::HashSet;
 
-use cryptopals::{aes, pad::pkcs7};
+use cryptopals::{
+    aes,
+    pad::{pkcs7, pkcs7_remove},
+};
 
 use data_encoding::BASE64;
 use insta::{assert_debug_snapshot, assert_snapshot};
@@ -37,7 +40,7 @@ fn challenge_10() {
 
 #[test]
 fn challenge_11() {
-    let encryption_oracle_1 = |plaintext| {
+    fn encryption_oracle_1(plaintext: &[u8]) -> Vec<u8> {
         let key = b"YELLOW SUBMARINE".into();
         let iv = b"RANDOM IV WOWOWW";
 
@@ -50,9 +53,9 @@ fn challenge_11() {
         padded_plaintext.resize(padded_plaintext.len() + padding_len, 0x5a);
 
         aes::cbc_encrypt(key, padded_plaintext.as_slice(), iv)
-    };
+    }
 
-    let encryption_oracle_2 = |plaintext| {
+    fn encryption_oracle_2(plaintext: &[u8]) -> Vec<u8> {
         let key = b"YELLOW SUBMARINE".into();
 
         // Random padding len, chosen by fair dice roll
@@ -64,7 +67,7 @@ fn challenge_11() {
         padded_plaintext.resize(padded_plaintext.len() + padding_len, 0x5a);
 
         aes::ecb_encrypt(key, padded_plaintext.as_slice())
-    };
+    }
 
     fn detect_cbc<'a>(encryption_oracle: impl Fn(&'a [u8]) -> Vec<u8>) -> bool {
         let plaintext = b"YELLOW SUBMARINEYELLOW SUBMARINEYELLOW SUBMARINE";
@@ -79,4 +82,125 @@ fn challenge_11() {
 
     assert!(detect_cbc(encryption_oracle_1));
     assert!(!detect_cbc(encryption_oracle_2));
+}
+
+#[test]
+fn challenge_12() {
+    fn encryption_oracle(plaintext: &[u8]) -> Vec<u8> {
+        let secret_text_base64 = include_str!("challenge-data/12.txt");
+        let secret_text = secret_text_base64
+            .lines()
+            .flat_map(|line| {
+                BASE64
+                    .decode(line.as_bytes())
+                    .expect("input should be valid base64")
+            })
+            .collect::<Vec<u8>>();
+        let key = b"YELLOW SUBMARINE".into();
+
+        let mut plaintext_plus_secret = Vec::from(plaintext);
+        plaintext_plus_secret.extend_from_slice(&secret_text);
+
+        aes::ecb_encrypt(key, &plaintext_plus_secret)
+    }
+
+    // Step 1
+    let initial_ciphertext_len = encryption_oracle(&[]).len();
+    let mut i = 1;
+    let (block_size, secret_len) = loop {
+        let len = encryption_oracle(&vec![0; i]).len();
+        if len != initial_ciphertext_len {
+            break (len - initial_ciphertext_len, len - 16 - i);
+        }
+        i += 1;
+    };
+    assert_eq!(16, block_size);
+
+    // Step 2
+    fn detect_ecb<'a>(encryption_oracle: impl Fn(&'a [u8]) -> Vec<u8>) -> bool {
+        let plaintext = b"YELLOW SUBMARINEYELLOW SUBMARINEYELLOW SUBMARINE";
+        let mut chunks = HashSet::new();
+        for chunk in encryption_oracle(plaintext).chunks_exact(16) {
+            if !chunks.insert(chunk) {
+                return true;
+            }
+        }
+        false
+    }
+    assert!(detect_ecb(encryption_oracle));
+
+    // Step 3
+    let mut plaintext = Vec::from(b"YELLOW SUBMARIN");
+    let first_ciphertext_block_with_one_secret_byte = &encryption_oracle(&plaintext)[..16];
+
+    // Step 4 + 5
+    for i in u8::MIN..=u8::MAX {
+        plaintext.push(i);
+        if &encryption_oracle(&plaintext)[..16] == first_ciphertext_block_with_one_secret_byte {
+            break;
+        }
+        plaintext.pop();
+    }
+    let first_secret_byte = plaintext[15];
+    assert_eq!(b'R', first_secret_byte);
+
+    // Step 6
+    //
+    // For this step I'm going to take a different approach. Rather than shortening my
+    // input to the oracle, I'm going to extend it. This way I can find secrets longer
+    // than a block size without changing techniques part way through, as the proposed
+    // approach would require.
+    //
+    // Assume:
+    // * secret: "SECRET"
+    // * block_len: 5
+    //
+    // The encryption oracle leaks the secret_len as calculated above.
+    //
+    // We know if we ask the oracle to encrypt (block_len - (secret_len % block_len))
+    // bytes then all blocks will be full and PKCS#7 will add a block of
+    // [5, 5, 5, 5, 5]. This isn't very helpful, but if we instead ask the oracle to
+    // encrypt one byte fewer than that then the final block plaintext will be
+    // ["T", 4, 4, 4, 4]. The value of the "T" would be unknown to us, but could be
+    // determined in no more than 256 guesses.
+    //
+    // We can then repeat that process (looking at blocks from the back):
+    //
+    // ["T", 4, 4, 4, 4]
+    // ["E", "T", 3, 3, 3]
+    // ["R", "E", "T", 2, 2]
+    // ["C", "R", "E", "T", 1]
+    // ["E", "C", "R", "E", "T"] [5, 5, 5, 5, 5]
+    let mut plaintext_of_secret = vec![];
+    'outer: for byte_index_to_break in (0..secret_len).rev() {
+        // We always want to make byte_index_to_break the first
+        // byte of a block.
+        let target_byte_position = byte_index_to_break.next_multiple_of(16);
+        let padding_required = target_byte_position - byte_index_to_break;
+        let block_index = target_byte_position / 16;
+        let cipher_block_to_match: Vec<u8> = encryption_oracle(&vec![0; padding_required])
+            .chunks_exact(16)
+            .nth(block_index)
+            .expect("ciphertext should be long enough")
+            .into();
+
+        // Insert arbitrary data for now, the value will be updated in
+        // the loop below.
+        plaintext_of_secret.insert(0, 0);
+        pkcs7(&mut plaintext_of_secret, 16);
+        for i in u8::MIN..=u8::MAX {
+            plaintext_of_secret[0] = i;
+            let cipher_block_to_check: Vec<u8> = encryption_oracle(&plaintext_of_secret)
+                .chunks_exact(16)
+                .next()
+                .expect("ciphertext should be long enough")
+                .into();
+            if cipher_block_to_check == cipher_block_to_match {
+                pkcs7_remove(&mut plaintext_of_secret);
+                continue 'outer;
+            }
+        }
+        panic!("should always find a valid solution after checking all bytes")
+    }
+    assert_snapshot!(String::from_utf8_lossy(&plaintext_of_secret));
 }
